dfe4af08383112e1953dd56bb42a3d649440f711
ark-go <ani.prog.go@gmail.com>
2021-09-06 17:44:22 +0300

    v0.0.2

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,6 +1,25 @@
 package main
 
-import()
+import (
+	"time"
 
-func init(){
+	"go.uber.org/zap"
+)
+
+func init() {
+
+}
+
+func main() {
+	logger, _ := zap.NewProduction()
+	defer logger.Sync() // flushes buffer, if any
+	sugar := logger.Sugar()
+
+	sugar.Infow("failed to fetch URL",
+		// Structured context as loosely typed key-value pairs.
+		"url", "http:://ccccccc",
+		"attempt", 3,
+		"backoff", time.Second,
+	)
+	sugar.Infof("Failed to fetch URL: %s--%s", "https://hhhhhh.uk", "kkkkk")
 }
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/ark-go/loggerZap
+
+go 1.17
+
+require (
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	go.uber.org/zap v1.19.0 // indirect
+)
